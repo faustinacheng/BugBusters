e19e95f2c8a7c922e7a0ff653f06b367351b0f94
Peter Ma <peterma1999@gmail.com>
2023-10-14 12:23:38 -0400

    Initialized spring framework

---
+++ src/test/java/com/example/reservationproject/ReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reservationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
